274d0deb7dda9120614c0e6dcd9d5a9e0a9ad7de
cornollc <cornollc@gmail.com>
2023-05-03 01:14:41 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# b7a10-chef-recipe-hunter-server-side-cornollc" 
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = 5000;
+
+const chef = require('./data/data.json');
+
+app.get('/', (req, res) => {
+    res.send('Chef is running')
+})
+
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+app.listen(port, () => {
+    console.log(`Chef is Running on port ${port}`)
+})
